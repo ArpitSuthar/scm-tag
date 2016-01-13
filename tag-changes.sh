f06cd2d3952e99a77889ec7dbaa7a6a013604b74
Arpit Suthar <nikhilsuthar@gmail.com>
2016-01-13 22:47:44 +0530

    Shell Script

--- tag-changes.sh
+++ tag-changes.sh
@@ -5,3 +5,12 @@ cd ../$1
 pwd1=`pwd`
 
 echo $pwd1
+
+changeList=`git diff --name-only | grep pom.xml`
+
+for file in $(git diff --name-only | grep pom.xml)
+do
+    git add $file
+done
+
+git commit -m "Updated POM Versions"
